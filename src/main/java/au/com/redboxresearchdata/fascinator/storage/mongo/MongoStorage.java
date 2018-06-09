666a8c6e157eede3b8c8e06c773563bb7c17ad94
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-06-09 16:01:14 +0930

    Added startup method to create dedicated views for each package type configured in the system so they can be easily queried.
    Added an aggregation function to allow more control over data queries

--- src/main/java/au/com/redboxresearchdata/fascinator/storage/mongo/MongoStorage.java
+++ src/main/java/au/com/redboxresearchdata/fascinator/storage/mongo/MongoStorage.java
@@ -39,6 +39,7 @@ import com.googlecode.fascinator.api.storage.DigitalObject;
 import com.googlecode.fascinator.api.storage.JsonDigitalObject;
 import com.googlecode.fascinator.api.storage.JsonStorage;
 import com.googlecode.fascinator.api.storage.StorageException;
+import com.googlecode.fascinator.common.JsonObject;
 import com.googlecode.fascinator.common.JsonSimple;
 import com.googlecode.fascinator.common.JsonSimpleConfig;
 import com.mongodb.MongoClient;
@@ -102,195 +103,212 @@ import com.mongodb.client.model.Aggregates;
  */
 public class MongoStorage implements JsonStorage {
 
-    /** System Config */
-    private JsonSimpleConfig systemConfig;
+	/** System Config */
+	private JsonSimpleConfig systemConfig;
 
-    private MongoClient mongoClient;
-    private MongoDatabase mongoDb;
-    private String defaultCollection;
-    private String objectMetadataCollectionName;
-    private String recordMetadataViewName;
+	private MongoClient mongoClient;
+	private MongoDatabase mongoDb;
+	private String defaultCollection;
+	private String objectMetadataCollectionName;
+	private String recordMetadataViewName;
 
-    private MongoDigitalObject.PayloadBackend payloadBackend;
+	private MongoDigitalObject.PayloadBackend payloadBackend;
 
-    @Override
-    public String getId() {
-        return "mongodb-storage";
-    }
-
-    @Override
-    public String getName() {
-        return "MongoDB Storage Plugin";
-    }
-
-    @Override
-    public PluginDescription getPluginDetails() {
-        return new PluginDescription(this);
-    }
-
-    @Override
-    public void init(File jsonFile) throws PluginException {
-        try {
-            systemConfig = new JsonSimpleConfig(jsonFile);
-            init();
-
-        } catch (IOException ioe) {
-            throw new StorageException("Failed to read file configuration!",
-                    ioe);
-        }
-    }
-
-    @Override
-    public void init(String jsonString) throws PluginException {
-        try {
-            systemConfig = new JsonSimpleConfig(jsonString);
-            init();
-        } catch (IOException ioe) {
-            throw new StorageException("Failed to read string configuration!",
-                    ioe);
-        }
-    }
-
-    private void init() {
-        String host = systemConfig.getString("localhost", "storage", "mongo",
-                "host");
-        int port = systemConfig.getInteger(27017, "storage", "mongo", "port")
-                .intValue();
-        String db = systemConfig.getString("redbox", "storage", "mongo", "db");
-        defaultCollection = systemConfig.getString("default", "storage",
-                "mongo", "defaultCollection");
-        objectMetadataCollectionName = systemConfig.getString("tf_obj_meta",
-                "storage", "mongo", "metadataCollection");
-        
-        recordMetadataViewName = systemConfig.getString("metadataDocuments",
-                "storage", "mongo", "recordMetadataCollection");
-
-        String payloadBackendName = systemConfig.getString("MONGO", "storage",
-                "mongo", "payload_backend");
-        payloadBackend = MongoDigitalObject.PayloadBackend
-                .valueOf(payloadBackendName);
-        mongoClient = new MongoClient(host, port);
-        mongoDb = mongoClient.getDatabase(db);
-        
-        createView();
-        
-    }
-
-    private void createView() {
-    	List pipeline = Arrays.asList(
-                BsonDocument.parse("{$match: {files: { $elemMatch:{ pid: 'metadata.tfpackage'}}}}"),
-                BsonDocument.parse("{ $project: { files: { '$filter': { input: '$files', as: 'files', cond: {$eq: ['$$files.pid','metadata.tfpackage']}}}}}"),
-                BsonDocument.parse("{ $unwind:  '$files' }"),
-                BsonDocument.parse("{ $project: { 'metadata':'$files.source.payload', 'redboxOid': '$files.oid'}}"),
-                BsonDocument.parse("{$lookup: { from: 'tf_obj_meta', localField:'redboxOid', foreignField: 'redboxOid', as: 'tfObj'}}"),
-                BsonDocument.parse("{ $addFields: { 'metadata': { 'redboxOid':  '$redboxOid', 'date_object_created':'$tfObj.date_object_created', 'date_object_modified':'$tfObj.date_object_modified' }}}"),
-                BsonDocument.parse("{ $replaceRoot: { newRoot: '$metadata'}}")
-        );
-    	 try {
-    	mongoDb.createView(this.recordMetadataViewName, this.defaultCollection, pipeline);
-    	 } catch (MongoCommandException e) {
-    		 //Error code 48 means that the view has already been created
-    		 if(e.getCode() != 48) {
-    			 throw e;
-    		 }
-    	 }
+	@Override
+	public String getId() {
+		return "mongodb-storage";
 	}
 
 	@Override
-    public void shutdown() throws PluginException {
-        mongoClient.close();
-    }
+	public String getName() {
+		return "MongoDB Storage Plugin";
+	}
 
-    @Override
-    public DigitalObject createObject(String oid) throws StorageException {
-        return createObject(oid, defaultCollection);
-    }
+	@Override
+	public PluginDescription getPluginDetails() {
+		return new PluginDescription(this);
+	}
 
-    public synchronized JsonDigitalObject createObject(String oid,
-            String collectionName) throws StorageException {
-        if (oid == null) {
-            throw new StorageException(
-                    "Cannot create object in storage with NULL oid");
-        }
-        // start with the object...
-        MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName,
-                objectMetadataCollectionName, oid, payloadBackend);
-        if (obj.existsInStorage()) {
-            throw new StorageException(
-                    "Error; object '" + oid + "' already exists in MongoDB");
-        }
-        obj.save();
-        return obj;
-    }
+	@Override
+	public void init(File jsonFile) throws PluginException {
+		try {
+			systemConfig = new JsonSimpleConfig(jsonFile);
+			init();
 
-    @Override
-    public DigitalObject getObject(String oid) throws StorageException {
-        return getObject(oid, defaultCollection);
-    }
+		} catch (IOException ioe) {
+			throw new StorageException("Failed to read file configuration!", ioe);
+		}
+	}
 
-    public JsonDigitalObject getObject(String oid, String collectionName)
-            throws StorageException {
-        MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName,
-                objectMetadataCollectionName, oid, payloadBackend);
-        obj.load();
-        return obj;
-    }
+	@Override
+	public void init(String jsonString) throws PluginException {
+		try {
+			systemConfig = new JsonSimpleConfig(jsonString);
+			init();
+		} catch (IOException ioe) {
+			throw new StorageException("Failed to read string configuration!", ioe);
+		}
+	}
 
-    @Override
-    public void removeObject(String oid) throws StorageException {
-        removeObject(oid, defaultCollection);
-    }
+	private void init() {
+		String host = systemConfig.getString("localhost", "storage", "mongo", "host");
+		int port = systemConfig.getInteger(27017, "storage", "mongo", "port").intValue();
+		String db = systemConfig.getString("redbox", "storage", "mongo", "db");
+		defaultCollection = systemConfig.getString("default", "storage", "mongo", "defaultCollection");
+		objectMetadataCollectionName = systemConfig.getString("tf_obj_meta", "storage", "mongo", "metadataCollection");
 
-    public void removeObject(String oid, String collectionName)
-            throws StorageException {
-        MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName,
-                objectMetadataCollectionName, oid, payloadBackend);
-        obj.remove();
-    }
+		recordMetadataViewName = systemConfig.getString("metadataDocuments", "storage", "mongo",
+				"recordMetadataCollection");
 
-    @Override
-    public Set<String> getObjectIdList() {
-        Set<String> objectIdList = new HashSet<String>();
-        MongoCollection<Document> objectMetaCol = mongoDb
-                .getCollection(objectMetadataCollectionName);
-        List<Document> objectIds = objectMetaCol.find(new Document())
-                .projection(fields(include("redboxOid"), excludeId()))
-                .into(new ArrayList<Document>());
-        for (Document doc : objectIds) {
-            objectIdList.add(doc.getString("redboxOid"));
-        }
-        return objectIdList;
-    }
+		String payloadBackendName = systemConfig.getString("MONGO", "storage", "mongo", "payload_backend");
+		payloadBackend = MongoDigitalObject.PayloadBackend.valueOf(payloadBackendName);
+		mongoClient = new MongoClient(host, port);
+		mongoDb = mongoClient.getDatabase(db);
 
-    public void dropDb() throws Exception {
-        mongoDb.drop();
-    }
-    
-    public JsonSimple pagedQuery(String collection, String filterString ) throws IOException{
-    	return pagedQuery(collection,filterString,0,10);
-    }
-    
-    public JsonSimple pagedQuery(String collection, String filterString, int startIndex, int rows ) throws IOException{
-    	
-    	List<BsonDocument> pipeline = Arrays.asList(
-                BsonDocument.parse("{$match:"+ filterString +"}"),
-                BsonDocument.parse("{'$group':{'_id': null, 'numFound': {'$sum': 1 }, 'docs':{ '$push':'$$ROOT' }} },"),
-                BsonDocument.parse("{'$project': { 'numFound':1 , 'docs' : {'$slice': ['$docs',"+startIndex+","+ rows+"] }   }}")
-        );
-    	
-    	AggregateIterable<Document> result =this.mongoDb.getCollection(collection).aggregate(pipeline);
-    	if(result.first() != null) {
-    		return new JsonSimple(result.first().toJson());
-    	} else {
-    		return  new JsonSimple("{\"_id\": null,  \"numFound\": 0,  \"docs\": []}");
-    	}
-    	
-    	
-    }
-    
-    public FindIterable<Document> query(String collection, String filterString){
-    	BsonDocument filter = BsonDocument.parse(filterString);
-    	return this.mongoDb.getCollection(collection).find(filter);
-    }
+		createMetadataDocumentsView();
+		createPackageViews();
+
+	}
+
+	/**
+	 * Create views for each package type to aid querying
+	 */
+	private void createPackageViews() {
+		
+		JsonObject packageTypes = systemConfig.getObject("portal", "packageTypes");
+		System.out.println(new JsonSimple(packageTypes).toString(true));
+		for (Object keyObject : packageTypes.keySet()) {
+			String key = (String) keyObject;
+			System.out.println("Creating view for key: " + key);
+			List pipeline = Arrays.asList(BsonDocument.parse("{$match: { 'metaMetadata.type' : '" + key + "'}}"));
+			if (key.equals("default") || key.equals(this.recordMetadataViewName)) {
+				key = key + "_package";
+			}
+			try {
+				mongoDb.createView(key, this.recordMetadataViewName, pipeline);
+			} catch (MongoCommandException e) {
+				// Error code 48 means that the view has already been created
+				if (e.getCode() != 48) {
+					throw e;
+				}
+			}
+
+		}
+
+	}
+
+	private void createMetadataDocumentsView() {
+		List pipeline = Arrays.asList(
+				BsonDocument.parse("{$match: {files: { $elemMatch:{ pid: 'metadata.tfpackage'}}}}"),
+				BsonDocument
+						.parse("{ $project: { files: { '$filter': { input: '$files', as: 'files', cond: {$eq: ['$$files.pid','metadata.tfpackage']}}}}}"),
+				BsonDocument.parse("{ $unwind:  '$files' }"),
+				BsonDocument.parse("{ $project: { 'metadata':'$files.source.payload', 'redboxOid': '$files.oid'}}"),
+				BsonDocument
+						.parse("{$lookup: { from: 'tf_obj_meta', localField:'redboxOid', foreignField: 'redboxOid', as: 'tfObj'}}"),
+				BsonDocument
+						.parse("{ $addFields: { 'metadata': { 'redboxOid':  '$redboxOid', 'date_object_created':'$tfObj.date_object_created', 'date_object_modified':'$tfObj.date_object_modified' }}}"),
+				BsonDocument.parse("{ $replaceRoot: { newRoot: '$metadata'}}"));
+		try {
+			mongoDb.createView(this.recordMetadataViewName, this.defaultCollection, pipeline);
+		} catch (MongoCommandException e) {
+			// Error code 48 means that the view has already been created
+			if (e.getCode() != 48) {
+				throw e;
+			}
+		}
+	}
+
+	@Override
+	public void shutdown() throws PluginException {
+		mongoClient.close();
+	}
+
+	@Override
+	public DigitalObject createObject(String oid) throws StorageException {
+		return createObject(oid, defaultCollection);
+	}
+
+	public synchronized JsonDigitalObject createObject(String oid, String collectionName) throws StorageException {
+		if (oid == null) {
+			throw new StorageException("Cannot create object in storage with NULL oid");
+		}
+		// start with the object...
+		MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName, objectMetadataCollectionName, oid,
+				payloadBackend);
+		if (obj.existsInStorage()) {
+			throw new StorageException("Error; object '" + oid + "' already exists in MongoDB");
+		}
+		obj.save();
+		return obj;
+	}
+
+	@Override
+	public DigitalObject getObject(String oid) throws StorageException {
+		return getObject(oid, defaultCollection);
+	}
+
+	public JsonDigitalObject getObject(String oid, String collectionName) throws StorageException {
+		MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName, objectMetadataCollectionName, oid,
+				payloadBackend);
+		obj.load();
+		return obj;
+	}
+
+	@Override
+	public void removeObject(String oid) throws StorageException {
+		removeObject(oid, defaultCollection);
+	}
+
+	public void removeObject(String oid, String collectionName) throws StorageException {
+		MongoDigitalObject obj = new MongoDigitalObject(mongoDb, collectionName, objectMetadataCollectionName, oid,
+				payloadBackend);
+		obj.remove();
+	}
+
+	@Override
+	public Set<String> getObjectIdList() {
+		Set<String> objectIdList = new HashSet<String>();
+		MongoCollection<Document> objectMetaCol = mongoDb.getCollection(objectMetadataCollectionName);
+		List<Document> objectIds = objectMetaCol.find(new Document())
+				.projection(fields(include("redboxOid"), excludeId())).into(new ArrayList<Document>());
+		for (Document doc : objectIds) {
+			objectIdList.add(doc.getString("redboxOid"));
+		}
+		return objectIdList;
+	}
+
+	public void dropDb() throws Exception {
+		mongoDb.drop();
+	}
+
+	public JsonSimple pagedQuery(String collection, String filterString) throws IOException {
+		return pagedQuery(collection, filterString, 0, 10);
+	}
+
+	public JsonSimple pagedQuery(String collection, String filterString, int startIndex, int rows) throws IOException {
+
+		List<BsonDocument> pipeline = Arrays.asList(BsonDocument.parse("{$match:" + filterString + "}"),
+				BsonDocument.parse("{'$group':{'_id': null, 'numFound': {'$sum': 1 }, 'docs':{ '$push':'$$ROOT' }} },"),
+				BsonDocument.parse("{'$project': { 'numFound':1 , 'docs' : {'$slice': ['$docs'," + startIndex + ","
+						+ rows + "] }   }}"));
+
+		AggregateIterable<Document> result = this.mongoDb.getCollection(collection).aggregate(pipeline);
+		if (result.first() != null) {
+			return new JsonSimple(result.first().toJson());
+		} else {
+			return new JsonSimple("{\"_id\": null,  \"numFound\": 0,  \"docs\": []}");
+		}
+	}
+
+	public FindIterable<Document> query(String collection, String filterString) {
+		BsonDocument filter = BsonDocument.parse(filterString);
+		return this.mongoDb.getCollection(collection).find(filter);
+	}
+	
+	public AggregateIterable<Document> aggregate(String collection, List<BsonDocument> pipeline) {
+		
+		return this.mongoDb.getCollection(collection).aggregate(pipeline);
+	}
 
 }
